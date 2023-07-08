70785e215e28fa75674f9c1fb992972b59524328
Devedse <davydavidse@gmail.com>
2023-07-08 13:22:02 +0200

    WIP auto get data from steam api

--- DeveLanCacheUI_SteamDepotFinder/Steam/SteamApi.cs
+++ DeveLanCacheUI_SteamDepotFinder/Steam/SteamApi.cs
@@ -17,12 +17,20 @@ namespace DeveLanCacheUI_SteamDepotFinder.Steam
             string path = Path.Combine("Steam", "SteamData.json");
             if (File.Exists(path))
             {
+                Console.WriteLine($"Found {path} so reading apps from that file.");
                 var json = File.ReadAllText(path);
                 return JsonSerializer.Deserialize<SteamApiData>(json);
             }
             else
             {
-                throw new FileNotFoundException($"File not found: {path}");
+                Console.WriteLine($"Could not find {path}, so obtaining new SteamApi Data...");
+                using var c = new HttpClient();
+                var result = c.GetAsync("https://api.steampowered.com/ISteamApps/GetAppList/v2/").Result;
+                var resultString = result.Content.ReadAsStringAsync().Result;
+                Console.WriteLine($"Writing result to file. First 1000 chars: {resultString.Substring(0, 1000)}");
+
+                File.WriteAllText(path, resultString);
+                return JsonSerializer.Deserialize<SteamApiData>(resultString);
             }
         }
     }
